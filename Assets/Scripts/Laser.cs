4e667085cf202ae523775b4bf09ba35f9124a0cb
Dean Fouche <26630822+deanfouche@users.noreply.github.com.>
2024-07-08 22:05:15 +0200

    Added ability to shoot a laser bullet

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        if (transform.position.y > 8)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 3.5f;
+    [SerializeField]
+    private float _speed = 5f;
+    private float _horizontalBoundary = 11.4f;
+    private float _bottomBoundary = -1f;
+    private float _topBoundary = 6f;
+
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private int _firingDelay = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +24,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         CalculateMovement();
+        ShootLaser();
     }
 
     void CalculateMovement()
@@ -25,22 +33,36 @@ public class Player : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
-        transform.Translate(direction * speed * Time.deltaTime);
+        transform.Translate(direction * _speed * Time.deltaTime);
 
 
         // Bounds for player y pos
-        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -1, 6), 0);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, _bottomBoundary, _topBoundary), 0);
 
         // Wrapping bounds for player x pos
-        if (transform.position.x > 11.4f)
+        if (transform.position.x > _horizontalBoundary)
         {
-            Vector3 newXPos = new Vector3(-11.4f, transform.position.y, transform.position.z);
+            Vector3 newXPos = new Vector3(-_horizontalBoundary, transform.position.y, transform.position.z);
             transform.position = newXPos;
         }
-        else if (transform.position.x < -11.4f)
+        else if (transform.position.x < -_horizontalBoundary)
         {
-            Vector3 newXPos = new Vector3(11.4f, transform.position.y, transform.position.z);
+            Vector3 newXPos = new Vector3(_horizontalBoundary, transform.position.y, transform.position.z);
             transform.position = newXPos;
+        }
+    }
+
+    void ShootLaser()
+    {
+        if (Input.GetKey(KeyCode.Space) && _firingDelay == 0)
+        {
+            _firingDelay = 10;
+            Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+        }
+        
+        if (_firingDelay > 0)
+        {
+            _firingDelay--;
         }
     }
 }
