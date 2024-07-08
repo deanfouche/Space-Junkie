dfbbb31b5ee01e5021e611d50f0517824879f742
Dean Fouche <26630822+deanfouche@users.noreply.github.com.>
2024-07-08 08:00:45 +0200

    Added player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,18 +5,46 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 3f;
+    private float _speed = 8f;
 
     // Start is called before the first frame update
     void Start()
     {
         // Set initial position
-        transform.position = new Vector3(0, 0, 0);
+        transform.position = new Vector3(0, 1, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+
+        // Bounds for player y pos
+        if (transform.position.y >= 6)
+        {
+            Vector3 newYPos = new Vector3(transform.position.x, 6, transform.position.z);
+            transform.position = newYPos;
+        } else if (transform.position.y <= -1)
+        {
+            Vector3 newYPos = new Vector3(transform.position.x, -1, transform.position.z);
+            transform.position = newYPos;
+        }
+
+        // Bounds for player x pos
+        if (transform.position.x > 15)
+        {
+            Vector3 newXPos = new Vector3(-15.0f, transform.position.y, transform.position.z);
+            transform.position = newXPos;
+        }
+        else if (transform.position.x < -15)
+        {
+            Vector3 newXPos = new Vector3(15f, transform.position.y, transform.position.z);
+            transform.position = newXPos;
+        }
     }
 }
